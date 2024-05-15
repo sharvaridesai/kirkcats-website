eb78063e84e9478f2b5aff7c1b2cf3790aa8d6d7
Sharvari Desai <desai.sharvari@gmail.com>
2024-05-15 18:50:16 -0400

    Setup for additional pages

--- _data/pages.js
+++ _data/pages.js
@@ -3,6 +3,7 @@ const client = require("../utils").contentfulClient;
 module.exports = async () => {
   const pages = await client.getEntries({
     content_type: "page",
+    order: "fields.navbarPosition",
   });
   console.log(pages.items);
   return pages.items;
